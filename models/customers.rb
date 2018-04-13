770ab58ce4b679a59a16a024bd39a1d53d1701fc
Daniel Childs <dc.1994@outlook.com>
2018-04-13 15:05:00 +0100

    create film method in customers

--- models/customers.rb
+++ models/customers.rb
@@ -49,6 +49,16 @@ class Customer
       SqlRunner.run(sql, values)
     end
 
-  
+    def film()
+      sql = "SELECT films.* FROM films INNER JOIN tickets
+       ON films.id = tickets.film_id
+       WHERE tickets.customer_id = $1;"
+      values = [@id]
+      film = SqlRunner.run(sql, values)
+      return Film.map_films(film)
+    end
+
+
+
 
 end #end of class
